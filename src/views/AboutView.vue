67e82433ca56ca9c30cc97ef9f2996017c479169
Daniel <daniel.h.farias2@gmail.com>
2023-04-20 16:41:04 -0500

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup>
+let user = "daniel";
+</script>
+<template>
+
+  <h1 class="Welcome">{{ user }}</h1>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
